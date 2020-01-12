f844d9f8c8cfc0581cfdd8b666fe940adb0b756b
Mako Kamiya <mako4kamiya@gmail.com>
2020-01-12 22:01:49 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root'static_pages#home'
-  get '/help' => 'static_pages#help'
-  get '/about' => 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
 end
