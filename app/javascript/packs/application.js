1a590f15dcecc92773ec127e9c893e68e29d12a5
Mako Kamiya <mako4kamiya@gmail.com>
2020-01-13 17:34:03 +0900

    finish setup fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+import "@fortawesome/fontawesome-free/js/all";
 import 'bootstrap'
 import '../src/application.scss'
