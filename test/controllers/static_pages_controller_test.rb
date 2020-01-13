f89fbaf0e662e49b1a317ae98751aa57ca217fc4
Mako Kamiya <mako4kamiya@gmail.com>
2020-01-13 15:27:56 +0900

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Makostagram"
   end
-
+  
   test "should get help" do
     get help_path
     assert_response :success
+    assert_select "title", "Help|Makostagram"
   end
-
+  
   test "should get about" do
     get about_path
     assert_response :success
+    assert_select "title", "About|Makostagram"
   end
 
 end
