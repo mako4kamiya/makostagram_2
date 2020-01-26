0e7eb48dea1fac19d81bba6881e5a44c645c547d
Mako Kamiya <mako4kamiya@gmail.com>
2020-01-26 14:31:40 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  root'static_pages#home'#あとでログイン画面に変える
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  root'sessions#new'
+  get   '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   get  '/signup', to: 'users#new'
   resources :users, except: [:new]
 end
